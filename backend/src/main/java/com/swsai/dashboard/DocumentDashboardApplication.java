9721cd825f4c33bff9d52bd9862f00141334c8b3
727724EUIT503-rgb <727724euit503@skcet.ac.in>
2026-05-29 14:40:21 +0530

    Initialize project structure

---
+++ backend/src/main/java/com/swsai/dashboard/DocumentDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.swsai.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentDashboardApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/swsai/dashboard/DocumentDashboardApplicationTests.java
@@ -0,0 +1,12 @@
+package com.swsai.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
